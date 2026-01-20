af3599ffc74e582def60d16a1b31f759bd7f04a7
Lazutkin Aleksandr <lazutkinan@ya.ru>
2026-01-20 18:57:09 +0300

    Контрольная точка: mission5_2 is done

--- models/trips_stat_age.sql
+++ models/trips_stat_age.sql
@@ -4,8 +4,8 @@ with
             date(t.started_at) as date,
             extract(year from t.started_at) - extract(year from u.birth_date) as age
         from
-            scooters_raw.trips as t
-            inner join scooters_raw.users as u on t.user_id = u.id
+            {{ ref("trips_prep") }} as t
+            inner join {{ source("scooters_raw", "users") }} as u on t.user_id = u.id
     ),
     count_cte as (
         select
